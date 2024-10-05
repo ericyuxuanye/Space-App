1deb59d97e18607dde15f0d9a576c30ea9f79f22
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2024-10-05 10:50:07 -0700

    Created a dataframe with correct columns

--- Data_Processing/Data_Processing.py
+++ Data_Processing/Data_Processing.py
@@ -2,12 +2,29 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('Planetary_Systems_Composition.csv', on_bad_lines='skip', comment='#')
+df = pd.read_csv('Data_Processing/Planetary_Systems_Composition.csv', on_bad_lines='skip', comment='#')
+print(df)
+#df = df.dropna()
+
+exoplanet_name = df['pl_name']
 
 lst = []
 
 for col in df.columns:
     lst.append(col)
-    print(col)
+    #print(col)
 
-print(len(lst))
+columns_to_keep = ['pl_name', 'hostname', 'sy_snum', 'sy_pnum', 'sy_mnum'
+                   'pl_orbper', 'pl_orbsmax', 'pl_rade', 'pl_radj', 'pl_bmasse',
+                   'pl_bmassj', 'pl_dens', 'pl_orbeccen', 'pl_eqt', 'pl_orbincl',
+                   'st_spectype', 'rastr', 'ra', 'decstr', 'dec', 'sy_dist', 'sy_plx']
+df_cleaned = [columns_to_keep]
+
+dataFrame = pd.DataFrame(df, columns=columns_to_keep)
+
+print(dataFrame)
+
+
+#print(len(lst))
+
+#print(exoplanet_name)

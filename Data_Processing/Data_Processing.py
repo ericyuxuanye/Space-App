c246a79f80d385c8870729b62f16275b6006b96a
Rohan <rpande@uw.edu>
2024-10-04 23:38:08 -0700

    Preliminary Data Collection

--- Data_Processing/Data_Processing.py
+++ Data_Processing/Data_Processing.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+df = pd.read_csv('Planetary_Systems_Composition.csv', on_bad_lines='skip', comment='#')
+
+lst = []
+
+for col in df.columns:
+    lst.append(col)
+    print(col)
+
+print(len(lst))

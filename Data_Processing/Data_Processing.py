608d463efec6e5555f416475a3d07997d921f9a0
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2024-10-05 11:51:20 -0700

    Changed some stuff

--- Data_Processing/Data_Processing.py
+++ Data_Processing/Data_Processing.py
@@ -3,15 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('Data_Processing/Planetary_Systems_Composition.csv', on_bad_lines='skip', comment='#')
-#print(df)
-
-exoplanet_name = df['pl_name']
-
-lst = []
-
-for col in df.columns:
-    lst.append(col)
-    #print(col)
 
 columns_to_keep = ['pl_orbper', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl',
                    'st_spectype', 'ra', 'dec', 'sy_dist', 'sy_plx']

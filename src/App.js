9a2ada8b72d866ba90a1fd53fd70990cdc9afad8
Eric Ye <yuxuan.smiley@gmail.com>
2024-10-05 23:02:59 -0700

    Dumb zoom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useRef, useState} from "react";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
-import { OrbitControls, Sphere } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Sphere } from "@react-three/drei";
 import {
   EffectComposer,
   Bloom,
@@ -16,6 +16,7 @@ import "./sidenav.css"
 
 export default function App() {
   const controlsRef = useRef()
+  const [position, setPosition] = useState([0, Math.cos(Math.PI / 4) * 100, Math.sin(Math.PI/4) * 100]);
   const [target, setTarget] = useState(
     [0, 0, 0]
   );  // Default camera position
@@ -28,14 +29,16 @@ export default function App() {
   const updateTarget = (t) => {
     setTarget(t);
     console.log("target", t);
+    setPosition([t[0], t[1] + Math.cos(Math.PI / 4) * 100, t[2] + Math.sin(Math.PI/4) * 100])
   }
   return (
     <div>
       <MySideNav classname="sidenav"></MySideNav>
     <Canvas
       style={{ background: "black", width: "100vw", height: "100vh" }}
-      camera={{ position: [0, Math.cos(Math.PI / 4) * 100, Math.sin(Math.PI/4) * 100], fov: 50 }}  // Set the initial camera position
+      //camera={{ position: [0, Math.cos(Math.PI / 4) * 100, Math.sin(Math.PI/4) * 100], fov: 50 }}  // Set the initial camera position
     >
+      <PerspectiveCamera makeDefault position={position} fov={50}/>
       <SystemView star={data.Sun} setTargetPosition={updateTarget} />
       
       <OrbitControls enablePan={false} target={target}/>

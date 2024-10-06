fc771065757c4006fcb934588fa75ae2333c1b5f
Rohan <rpande@uw.edu>
2024-10-06 01:37:45 -0700

    sdfgh

--- Data_Processing/Data_Processing.py
+++ Data_Processing/Data_Processing.py
@@ -2,8 +2,14 @@ import pandas as pd
 import json
 import numpy as np
 from algo import signal_noise_ratio, limiting_distance, habitable_zone_lim, habitable
+import xgboost as xgb
 
-df = pd.read_csv('Data_Processing/Planetary_Systems_Composition.csv', on_bad_lines='skip', comment='#')
+model = xgb.XGBRegressor()
+model.load_model("./XGBoost_model.json")
+
+df_og = pd.read_csv('Planetary_Systems_Composition.csv', on_bad_lines='skip', comment='#')
+
+df = pd.read_csv('Planetary_Systems_Composition.csv', on_bad_lines='skip', comment='#')
 
 df = pd.DataFrame(df, columns=['st_teff', 'st_lum', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl', 'st_spectype', 'ra', 'dec', 'sy_dist', 'sy_plx', 'pl_name', 'hostname', 'pl_rade', 'st_rad'])
 
@@ -21,6 +27,18 @@ df = df.rename(columns={
                     'pl_rade': 'Exoplanet Radius (Earth Units)',
                     'st_rad': 'Stellar Radius'
 })
+for index, row in df_og.iterrows():
+    
+    if np.isnan(row['st_lum']):
+        # Predict luminosity using the awesome XGBoost model
+        row['st_lum'] = model.predict(row[[
+             'st_teff',
+             'st_mass',
+             'st_met',
+             'st_logg',
+             'st_dens',
+             'st_vsin',
+             'st_rad']].to_numpy().reshape(1, -1))[0]
 
 grouped_by_star = {}
 habitable_rows = {}
@@ -37,7 +55,8 @@ for index, row in df.iterrows():
             "Spectral Type": row['Spectral Type'],
             "Stellar Radius": row['Stellar Radius'],
             "Inclination": row['Inclination'],
-            "Habitable-Zone": habitable(row['Orbital Semi-Major Axis'], habitable_val[0], habitable_val[1]),
+            "Habitable-Zone-lower": habitable_val[0],
+            "Habitable-Zone-upper": habitable_val[1],
             "Planets": {}
         }
     
@@ -55,9 +74,9 @@ for index, row in df.iterrows():
             habitable_rows[star_name] = grouped_by_star[star_name]
 
 json_data_habitable = json.dumps(grouped_by_star, indent=4)
-json_data = open("./src/planetarydata.json","w")
+json_data = open("../src/planetarydata.json","w")
 json_data.write(json_data_habitable)
 json_data.close
 
 # json_data = json.dumps(grouped_by_star, indent=4)
-# print(json_data)
+# print(json_data

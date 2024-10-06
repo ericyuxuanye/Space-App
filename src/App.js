1493cf423ddbe529a20c316344e18e5f0e1ac35f
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2024-10-05 20:55:29 -0700

    Added a SidePanel

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import {
 } from "@react-three/postprocessing";
 import SystemView from "./SystemView";
 import data from "./data.json";
+import MySideNav from "./Menu";
 import * as THREE from 'three';
+import "./sidenav.css"
 
 export default function App() {
   return (
+    <div>
+      <MySideNav classname="sidenav"></MySideNav>
     <Canvas
       style={{ background: "black", width: "100vw", height: "100vh" }}
       camera={{ fov: 50, position: [0, Math.cos(Math.PI / 4) * 100, Math.sin(Math.PI/4) * 100] }}
@@ -19,5 +23,6 @@ export default function App() {
       <SystemView star={data.Sun} />
       <OrbitControls enablePan={false} />
     </Canvas>
+    </div>
   );
 }
